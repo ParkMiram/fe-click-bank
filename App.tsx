58db7e6ee8057859eb92877f847c0358a2df6a95
Sujin1517 <11tnwls12@gmail.com>
2024-07-10 16:53:13 +0900

    [Feat]: Add login step page(#2)

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginPage from './page/login/LoginPage';
 import KakaoLogin from './page/login/KakaoLogin';
@@ -6,6 +5,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashLogo from './page/splash/SplashLogo';
 import ClickHome from './page/main/ClickHome';
+import TermOfUse from './page/login/TermOfUse';
+import SetPassword from './page/login/SetPassword';
 
 const Stack = createStackNavigator();
 
@@ -19,13 +20,12 @@ export default function App() {
                 <Stack.Screen name="Splash" component={SplashLogo} />
                 <Stack.Screen name="Login" component={LoginPage} />
                 <Stack.Screen name="KakaoLogin" component={KakaoLogin} />
+                <Stack.Screen name="UserTermOfUse" component={TermOfUse} />
+                <Stack.Screen name="UserSetPassword" component={SetPassword} />
+                {/* <Stack.Screen name="UserSetPasswd" component={} /> */}
                 <Stack.Screen name="ClickHome" component={ClickHome} />
             </Stack.Navigator>
         </NavigationContainer>
-        // <View style={styles.container}>
-        //     <LoginPage />
-        //     <StatusBar style="auto" />
-        // </View>
     );
 }
 
