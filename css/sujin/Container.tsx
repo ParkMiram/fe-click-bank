f49c77db6e84536f4a5afb3868b300858dad8274
Sujin1517 <11tnwls12@gmail.com>
2024-07-10 16:51:36 +0900

    [Feat]: divide css(#2)

---
+++ css/sujin/Container.tsx
@@ -0,0 +1,18 @@
+import { Platform, StatusBar, StyleSheet } from "react-native";
+
+const Container = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    innerContainer: {
+        flex: 1,
+        width: "100%",
+        marginTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export { Container }
