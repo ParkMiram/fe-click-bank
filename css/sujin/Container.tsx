23c83c4117077661bae20acd270f3ac7fe1d2daf
Sujin1517 <11tnwls12@gmail.com>
2024-07-11 12:30:21 +0900

    [Design]: fix bg color

--- css/sujin/Container.tsx
+++ css/sujin/Container.tsx
@@ -3,6 +3,7 @@ import { Platform, StatusBar, StyleSheet } from "react-native";
 const Container = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
