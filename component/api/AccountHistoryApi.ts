827ca49f609a132f3823d2ae66adf1949bdd1e66
KJM99 <2098kjm@naver.com>
2024-07-12 11:43:31 +0900

    [Feat] AccountHistoryDetail 거래 내역 불러오기

--- component/api/AccountHistoryApi.ts
+++ component/api/AccountHistoryApi.ts
@@ -4,3 +4,7 @@ import {api} from "../../config/network";
 export const getAccountHistory = async (account: string):Promise<AxiosResponse<any>> => {
     return await api(`/api/v1/histories?account=${account}`, "get");
 }
+
+export const getAccountHistoryDetail = async (id: number):Promise<AxiosResponse<any>> => {
+    return await api(`/api/v1/histories/detail/${id}`, "get");
+}
