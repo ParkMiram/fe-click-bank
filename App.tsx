8b9a0c1c0885eb4c573224d2c42bfd64acbec48e
BoonDoPark <BoonDoPark>
2024-07-12 10:34:26 +0900

    [Fix] png를 svg로 변경

--- App.tsx
+++ App.tsx
@@ -45,7 +45,7 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator 
-                initialRouteName="Splash"
+                initialRouteName="Transfer"
                 screenOptions={{headerShown: false}}
             >
                 <Stack.Screen name="Transfer" component={Transfer} />
