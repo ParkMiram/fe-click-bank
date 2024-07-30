4f511cf052966611a988ac73619d80edc39b9f32
lsy <sylim0513@gmail.com>
2024-07-30 09:30:32 +0900

    feature/cardList

---
+++ page/card/CardPassword.tsx
@@ -0,0 +1,127 @@
+import { Alert,TouchableOpacity,Text,Dimensions, Platform, SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
+import Keypad from '../../component/auth/Keypad';
+import { useState } from 'react';
+
+export default function CardPassword( { route, navigation }: any ) {
+    const [password, setPassword] = useState("");
+    const [toStar, setStar] = useState("");
+    const [infoText, setInfoText] = useState("");
+
+    const handleNextPress = () => {
+        if (password.length === 4) {
+            navigation.navigate("CardComplete");
+        } else {
+            Alert.alert("경고", "비밀번호 4자리를 입력해 주세요.");
+        }
+    }
+    const addPassword = async (str: string) => {
+        if (password.length >= 4) return;
+        setPassword(password + str);
+        setStar("●".repeat(password.length+1));
+        if (password.length == 3) {
+  
+        }
+    }
+        const removePassword = () => {
+            if (password.length == 0) return;
+            setInfoText("");
+            setPassword(password.slice(0, password.length-1));
+            setStar("●".repeat(password.length-1));
+        }
+
+
+    
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.innerContainer}>
+            {/* <View style={styles.nameContainer}>
+                        <Text style={styles.cardText}>카드 생성</Text>
+            </View> */}
+            <View style={styles.password}>
+                <Text style={styles.cardPassword}>카드 비밀번호</Text>
+            </View>
+                
+            <View style={styles.passwordBox}>
+                <Text style={styles.passwordStar}>{toStar}</Text>
+                <Text>{infoText}</Text>
+            </View>
+                <Keypad numberKeyEvent={addPassword} backKeyEvent={removePassword}/>
+                <TouchableOpacity style={styles.applyButton} onPress={handleNextPress}>
+                    <Text style={styles.applyButtonText}>다음</Text>
+      </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    innerContainer: {
+        flex: 1,
+        width: "100%",
+        marginTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    
+    },
+    passwordBox: {
+        flex: 1,
+        width: "50%",
+        height:50,
+        // paddingTop: "5%",
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    passwordStar: {
+        width: '100%',
+        letterSpacing: 12,
+        paddingBottom: 16,
+        paddingTop: 28,
+        fontSize: 24,
+        fontWeight:'600',
+        textAlign: 'center',
+        borderBottomWidth: 2,
+        borderColor: "#000"
+    },
+    nameContainer: {
+        width: '85%',
+        flexDirection: 'row',
+        marginTop: 15,
+        justifyContent: 'flex-start',
+    },
+    cardText: {
+        fontSize: 30,
+        // marginLeft: 20,
+    },
+    password:{
+        width: '85%',
+        flexDirection: 'row',
+        marginTop: 50,
+        // marginBottom:50,
+        justifyContent: 'center',
+
+    },
+    cardPassword:{
+        fontSize:25,
+       textAlign:'center'
+     },
+     applyButton: {
+        backgroundColor: '#B7E1CE',
+        padding: 15,
+        marginVertical:10,
+        borderRadius: 10,
+        alignItems: 'center',
+        width:'85%',
+        // position:'absolute',
+        // marginTop:50
+      },
+      applyButtonText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'black'
+      },
+});
