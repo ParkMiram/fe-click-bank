c671edc1903b528a3d769e4148ccc2096def003a
KJM99 <2098kjm@naver.com>
2024-08-01 17:43:01 +0900

    [Feat] 내 카드 목록, 내 카드 정보, 카드 상품 목록, 카드 상품 정보 Get 요청 통신 완료

---
+++ component/api/CardListApi.ts
@@ -0,0 +1,76 @@
+import {AxiosResponse} from "axios";
+import {apiCardList} from "../../config/network";
+
+export const getAllMyCard = async (token : any) : Promise<AxiosResponse<any>> => {
+    const query = `
+        query {
+            getAllMyCard {
+                cardId
+                cardName 
+                cardProduct{
+                    cardImg
+                }
+            }
+        }
+    `;
+
+    const body = { query };
+    return await apiCardList(`/graphql`, "post", body, {"Authorization" : "Bearer "+token});
+}
+
+export const getAllCardProduct = async ():Promise<AxiosResponse<any>> => {
+    const query = `
+        query {
+            getAllCardProduct {
+                cardProductId
+                cardProductName 
+                cardImg
+            }
+        }
+    `;
+
+    const body = { query };
+    return await apiCardList(`/graphql`, "post", body );
+}
+
+// {"Authorization" : "Bearer "+token}
+
+export const getCardProduct = async (id: number):Promise<AxiosResponse<any>> => {
+    const query = `
+        query {
+            getCardProduct(cardProductId: ${id}) {
+                cardProductId
+                cardProductName
+                cardAnnualFee
+                cardImg
+                cardBenefits
+            }
+        }
+    `;
+
+    const body = { query };
+    return await apiCardList(`/graphql`, "post", body);
+}
+
+export const getMyCard = async (id: number):Promise<AxiosResponse<any>> => {
+    const query = `
+        query {
+            getMyCard(cardId: ${id}) {
+                cardId
+                cardName
+                cardNumber
+                account
+                cardCVC
+                cardMonthLimit
+                cardAnnualFee
+                cardProduct{
+                  cardImg
+                  cardBenefits
+                }
+            }
+        }
+    `;
+
+    const body = { query };
+    return await apiCardList(`/graphql`, "post", body);
+}
