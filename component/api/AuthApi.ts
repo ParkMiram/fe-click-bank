1240fd2079a9cd34f9076fd71d6a27d656f57d9c
Sujin1517 <11tnwls12@gmail.com>
2024-08-16 14:46:01 +0900

    [Fix]: Hotfix main account check

--- component/api/AuthApi.ts
+++ component/api/AuthApi.ts
@@ -3,9 +3,9 @@ import { api } from "../../config/network";
 import { UserInfo } from "../../types/AuthTypes";
 
 export const getUserInfo = async (token:string):Promise<AxiosResponse<UserInfo>> => {
-    return await api(`api/v1/auth/token/${token}`, "get", {}, {});
+    return await api(`api/v1/auth/token/${token}`, "get");
 }
 
 export const setMainAccount = async (id:string, account:any):Promise<AxiosResponse<any>> => {
-    return await api(`api/v1/auth/${id}/main-account`, "put", account, {});
+    return await api(`api/v1/auth/${id}/main-account`, "put", account);
 }
